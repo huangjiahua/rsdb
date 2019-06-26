fa44896782c108cb46193e4fdb859a7ca6eadccd
huangjiahua <hjh4477@outlook.com>
2019-06-26 13:49:06 +0800

    Change the basic test.

--- test/basic_test.cpp
+++ test/basic_test.cpp
@@ -13,14 +13,17 @@ int main() {
     cout << "Is DB set up? " << boolalpha << db.Valid() << endl;
     assert(db.Valid());
 
-    cout << "Put k-v -> <key, value>" << endl;
-    status = db.Put(string("key"), string("value"), WriteOptions());
-    assert(status);
+    for (int i = 0; i < 10000; i++) {
+        std::string key = string("key") + to_string(i);
+        cout << "Put k-v -> <" << key << ", value>" << endl;
+        status = db.Put(key, string("value"), WriteOptions());
+        assert(status);
 
-    std::string value;
-    status = db.Get(string("key"), &value);
-    assert(status);
-    cout << "Get value -> " << value << endl;
+        std::string value;
+        status = db.Get(key, &value);
+        assert(status);
+        cout << "Get value -> " << value << endl;
+    }
 
 
     return 0;
